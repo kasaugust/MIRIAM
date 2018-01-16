81d8d69174f1a37a2152c0d20edbcad85af18f3d
kasaugust <35238971+kasaugust@users.noreply.github.com>
2018-01-16 23:00:32 +0100

    Add files via upload
    Folder z nagraniami do modelu
    Tworzenie modelu i funkcja do rozpoznania

---
+++ miriam_GMM.py
@@ -0,0 +1,59 @@
+#componentów 4-8
+#ramkowanie 10/25 ms
+#mfcc 13
+#delty! python_speech_features.base.delta()
+import scipy.io.wavfile as wav
+import fnmatch
+import os
+import pickle
+import numpy
+from python_speech_features import mfcc
+from python_speech_features import base
+from sklearn.mixture import GaussianMixture as GMM
+
+recordings=6
+ramkowanie=0.02
+components=4
+delta_memory=3
+path = os.getcwd()
+allMFCC = numpy.empty([1, recordings], dtype=object)
+i=0
+for file in os.listdir(os.path.join(path, 'miriam')):
+    fs, samples = wav.read(os.path.join(path, 'miriam', file))
+    MFCC = mfcc(samples, fs,winlen=ramkowanie, nfft=round(ramkowanie * fs) + 1, numcep=13)
+    delta=base.delta(MFCC, delta_memory)
+    delta_delta=base.delta(delta, delta_memory)
+    MFCC_and_deltas=numpy.c_[MFCC,delta,delta_delta]
+    allMFCC[0, i] = MFCC_and_deltas
+    i = i + 1
+# w nazwie pliku jest liczba parametrow mfcc z ktorej zostala funkcja wywołana
+#with open('mfcc_miriam.bin', 'wb') as f:
+#    pickle.dump(allMFCC, f)
+#    f.close()
+
+model= GMM(components, covariance_type="diag")
+tmp_data = numpy.zeros((0, 39))
+for j in range(0, recordings):
+    tmp_data = numpy.r_[tmp_data, allMFCC[0, j]]
+model.fit(tmp_data)
+with open('model.bin', 'wb') as f:
+    pickle.dump(model, f)
+    f.close()
+def if_Miriam(ramkowanie, delta_memory):
+    treshold = -50
+    path="miriam/1.wav"
+    with open('model.bin', 'rb') as f:
+        model=pickle.load(f)
+        f.close()
+    fs, data = wav.read(path)
+    MFCC = mfcc(samples, fs,winlen=ramkowanie, nfft=round(ramkowanie * fs) + 1, numcep=13)
+    delta=base.delta(MFCC, delta_memory)
+    delta_delta=base.delta(delta, delta_memory)
+    MFCC_and_deltas = numpy.c_[MFCC, delta, delta_delta]
+    score=model.score(MFCC_and_deltas)
+    print(score)
+    if score>treshold:
+        return True
+    else:
+        return False
+print(if_Miriam(ramkowanie,delta_memory))
